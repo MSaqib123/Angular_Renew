80e50fea80f0a81b013e75999aa681541e76bcb7
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-12-07 09:42:52 +0500

    [Section 1]

--- section-1-getting-started/src/app/app.component.ts
+++ section-1-getting-started/src/app/app.component.ts
@@ -8,5 +8,17 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'section-1-getting-started';
+  number = 0;
+  increment() {
+    this.number++;
+  }
+
+
+  /*
+  
+  ✔ {{ number }}
+This is interpolation (data binding from Component → HTML).
+What interpolation does?
+{{ }} is used to display a variable's value from the component inside the HTML view.
+*/
 }
